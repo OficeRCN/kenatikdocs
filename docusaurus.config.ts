27fbd29922458e80b29ec3fc5456691a37035ae0
OficeRCN <143032637+OficeRCN@users.noreply.github.com>
2024-02-07 18:31:54 -0500

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,18 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://oficercn.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/kenatikdocs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Red Nueva Conexión', // Usually your GitHub org/user name.
+  projectName: 'kenatikdocs', // Usually your repo name.
+
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
